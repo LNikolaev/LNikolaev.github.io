2fcd04f59842755c56bd61ab5205f4dbecc2485e
Luben Nikolaev <luben.lubenov@gmail.com>
2017-05-19 20:08:42 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$(".footerDetails div").removeClass("animate");
+   $(".center-linkedin").mouseenter(function(){       
+       $(".social-footer .center-linkedin").addClass("animate");
+   });
+     $(".center-download").mouseenter(function(){       
+       $(".social-footer .center-download").addClass("animate");
+   });
+});
