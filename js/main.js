8cdf8f25db88996893b146a82404307aca08298d
Luben Nikolaev <luben.lubenov@gmail.com>
2017-05-23 18:01:05 +0300

    adding animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-$(".footerDetails div").removeClass("animate");
-   $(".center-linkedin").mouseenter(function(){       
+
+$(document).ready(function() {
+	setTimeout(function(){
+		$('body').addClass('loaded');
+	}, 2000);
+    
+    $(".footerDetails div").removeClass("animate");
+    $(".center-linkedin").mouseenter(function(){
        $(".social-footer .center-linkedin").addClass("animate");
-   });
-     $(".center-download").mouseenter(function(){       
+    });
+     $(".center-download").mouseenter(function(){
        $(".social-footer .center-download").addClass("animate");
-   });
+    });
 });
