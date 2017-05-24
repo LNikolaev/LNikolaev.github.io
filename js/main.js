ae663e2c2c379969073360c6f1bb934546fe6828
Luben Nikolaev <luben.lubenov@gmail.com>
2017-05-24 14:25:59 +0300

    adding popup at footer icons/remove some fonts

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@
 $(document).ready(function() {
 	setTimeout(function(){
 		$('body').addClass('loaded');
-	}, 2000);
+	}, 0);
     
     $(".footerDetails div").removeClass("animate");
+    $(".download-text").hide();
+    $(".linkedin-text").hide();
     $(".center-linkedin").mouseenter(function(){
        $(".social-footer .center-linkedin").addClass("animate");
+        $(".linkedin-text").show();
     });
      $(".center-download").mouseenter(function(){
        $(".social-footer .center-download").addClass("animate");
+         $(".download-text").show();
+    });
+    $(".center-download").mouseleave(function(){
+        $(".download-text").hide();
+    });
+    $(".center-linkedin").mouseleave(function(){
+        $(".linkedin-text").hide();
     });
 });
